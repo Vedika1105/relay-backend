35bc0d015f24c19a192613aa4d7eb0d7e7d92f94
Vedika1105 <jainvedika774@gmail.com>
2026-06-19 08:05:43 +0530

    fix: move os.environ.setdefault to top of asgi.py

--- config/asgi.py
+++ config/asgi.py
@@ -2,14 +2,16 @@ import os
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-from django.core.asgi              import get_asgi_application
+from django.core.asgi import get_asgi_application
+
+# This must be called before any app imports to ensure Django is fully loaded
+django_asgi_app = get_asgi_application()
+
 from channels.routing              import ProtocolTypeRouter, URLRouter
 from channels.security.websocket   import AllowedHostsOriginValidator
 from django.conf                   import settings
 from apps.messaging.routing        import websocket_urlpatterns
 from apps.messaging.middleware     import JWTWebSocketMiddleware
-
-django_asgi_app = get_asgi_application()
 
 websocket_application = JWTWebSocketMiddleware(
     URLRouter(websocket_urlpatterns)
